769c6f02bababb774a3af3892e8854ed1725f655
Brad <114950577+goblinman617@users.noreply.github.com>
2023-10-25 14:49:33 -0400

    init commit

---
+++ CrudApp/app/src/main/java/com/example/crudapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.crudapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//view all current employees
+//can go to add employee
+//clicking on employee will
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
